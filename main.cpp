d877fe3e19f4524ff2bb421dda43059cb134680f
maru0522 <k021g1258@g.neec.ac.jp>
2022-05-18 15:01:07 +0900

    CG2授業資料04_01　まで

--- main.cpp
+++ main.cpp
@@ -299,24 +299,30 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// 値を書き込むと自動的に転送される
 	constMapMaterial->color = XMFLOAT4(1.0f, 0.0f, 0.0f, 0.5f);		// RGBAで半透明の赤
 
+	// 頂点データ構造体
+	struct Vertex
+	{
+		XMFLOAT3 pos;	// xyz座標
+		XMFLOAT2 uv;	// uv座標
+	};
 
 	// 頂点データ
-	XMFLOAT3 vertices[] = {
-	{ -0.5f, -0.5f, 0.0f }, // 左下 インデックス0
-	{ -0.5f, +0.5f, 0.0f }, // 左上 インデックス1
-	{ +0.5f, -0.5f, 0.0f }, // 右下 インデックス2
-	{ +0.5f, +0.5f, 0.0f }, // 右上 インデックス3
+	Vertex vertices[] = {
+		{{ -0.4f, -0.7f, 0.0f },{0.0f, 1.0f}}, // 左下 インデックス0
+		{{ -0.4f, +0.7f, 0.0f },{0.0f, 0.0f}}, // 左上
+		{{ +0.4f, -0.7f, 0.0f },{1.0f, 1.0f}}, // 右下
+		{{ +0.4f, +0.7f, 0.0f },{1.0f, 0.0f}}, // 右上
 	};
 
 	// インデックスデータ
-	uint16_t indices[] =
+	unsigned short indices[] =
 	{
 		0,1,2,	//三角形1つ目
 		1,2,3,	//三角形2つ目
 	};
-	
+
 	// 頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
-	UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
+	UINT sizeVB = static_cast<UINT>(sizeof(vertices[0]) * _countof(vertices));
 
 	// 頂点バッファの設定
 	D3D12_HEAP_PROPERTIES heapProp{}; // ヒープ設定
@@ -343,7 +349,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	assert(SUCCEEDED(result));
 
 	// GPU上のバッファに対応した仮想メモリ(メインメモリ上)を取得
-	XMFLOAT3* vertMap = nullptr;
+	Vertex* vertMap = nullptr;
 	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
 	assert(SUCCEEDED(result));
 	// 全頂点に対して
@@ -360,7 +366,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// 頂点バッファのサイズ
 	vbView.SizeInBytes = sizeVB;
 	// 頂点1つ分のデータサイズ
-	vbView.StrideInBytes = sizeof(XMFLOAT3);
+	vbView.StrideInBytes = sizeof(vertices[0]);
 
 
 	// インデックスデータ全体のサイズ
@@ -460,11 +466,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 #pragma region 頂点レイアウト
 	// 頂点レイアウト
 	D3D12_INPUT_ELEMENT_DESC inputLayout[] = {
-	{
-	"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,
-	D3D12_APPEND_ALIGNED_ELEMENT,
-	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0
-	}, // (1行で書いたほうが見やすい)
+		{ // xyz座標(1行で書いたほうが見やすい)
+			"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,
+			D3D12_APPEND_ALIGNED_ELEMENT,
+			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0
+		},
+		{ // uv座標(1行で書いたほうが見やすい)
+			"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0,
+			D3D12_APPEND_ALIGNED_ELEMENT,
+			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0
+		},
 	};
 #pragma endregion
 
