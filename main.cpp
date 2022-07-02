1415f8c8d291fe7942dc759a908dbe4ecd99dfe7
maru0522 <k021g1258@g.neec.ac.jp>
2022-07-02 19:56:03 +0900

    06_03途中

--- main.cpp
+++ main.cpp
@@ -363,25 +363,48 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     // インデックスデータ
     unsigned short indices[] =
     {
-    // 前
-        0,1,2,      //三角形1つ目
-        2,1,3,      //三角形2つ目
-    // 後
-        4,6,5,      //三角形3つ目
-        6,7,5,      //三角形4つ目
-    // 左
-        8,9,10,     //三角形5つ目
-        10,9,11,    //三角形6つ目
-    // 右
-        12,14,13,   //三角形7つ目
-        14,15,13,   //三角形8つ目
-    // 上
-        16,18,17,   //三角形9つ目
-        18,19,17,   //三角形10つ目
-    // 下
-        20,21,22,   //三角形11つ目
-        22,21,23,   //三角形12つ目
+        // 前
+            0,1,2,      //三角形1つ目
+            2,1,3,      //三角形2つ目
+        // 後
+            4,6,5,      //三角形3つ目
+            6,7,5,      //三角形4つ目
+        // 左
+            8,9,10,     //三角形5つ目
+            10,9,11,    //三角形6つ目
+        // 右
+            12,14,13,   //三角形7つ目
+            14,15,13,   //三角形8つ目
+        // 上
+            16,18,17,   //三角形9つ目
+            18,19,17,   //三角形10つ目
+        // 下
+            20,21,22,   //三角形11つ目
+            22,21,23,   //三角形12つ目
     };
+
+    for (int i = 0; i < _countof(indices) / 3; i++) {
+        // 三角形1つごとに計算していく
+        // 三角形のインデックスを取り出して、一時的な変数に入れる
+        unsigned short indexZero = indices[i * 3 + 0];
+        unsigned short indexOne = indices[i * 3 + 1];
+        unsigned short indexTwo = indices[i * 3 + 2];
+        // 三角形を構成する頂点座標をベクトルに代入
+        XMVECTOR p0 = XMLoadFloat3(&vertices[indexZero].pos);
+        XMVECTOR p1 = XMLoadFloat3(&vertices[indexOne].pos);
+        XMVECTOR p2 = XMLoadFloat3(&vertices[indexTwo].pos);
+        // p0->p1ベクトル、p0->p2ベクトルを計算（ベクトルの減算）
+        XMVECTOR v1 = XMVectorSubtract(p1, p0);
+        XMVECTOR v2 = XMVectorSubtract(p2, p0);
+        // 外積は両方から垂直なベクトル
+        XMVECTOR normal = XMVector3Cross(v1, v2);
+        // 正規化（長さを1にする）
+        normal = XMVector3Normalize(normal);
+        // 求めた法線を頂点データに代入
+        XMStoreFloat3(&vertices[indexZero].normal, normal);
+        XMStoreFloat3(&vertices[indexOne].normal, normal);
+        XMStoreFloat3(&vertices[indexTwo].normal, normal);
+    }
 
     // 頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
     UINT sizeVB = static_cast<UINT>(sizeof(vertices[0]) * _countof(vertices));
