a31e97f31eae96dc6a29f1b4fcc931370de6816e
maru0522 <k021g1258@g.neec.ac.jp>
2022-09-23 23:30:35 +0900

    Util.h/.cpp を追加し、文字列変換の関数を記載。 Window.classを追加。 v02でnamespace領域で記述した関数を文字列変換関数へ置換。 main.cpp は未対応。

---
+++ Window.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <Windows.h>        // CG2_01_01   12枚目
+
+class Window
+{
+public: // 静的変数
+    // ウィンドウサイズ
+    static const size_t width = 1280;
+    static const size_t height = 720;
+
+    // ウィンドウクラス名
+    static const wchar_t windowClassName[];
+
+public: // 静的関数
+    static LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+public: // 関数
+    Window();
+    Window(const char* title);
+    void DelWindow();
+
+    // ウィンドウの表示状態を変更する関数
+    // デフォルト:nCmdShow = SW_SHOW
+    void SetDisplay(int nCmdShow);
+
+    // ウィンドウ固定用のメッセージループ処理
+    // WM_QUITならFALSEを返す
+    bool IsKeep();
+
+    // ウィンドウハンドルの取得
+    // const: 引き渡しするメンバ変数のメンバ関数内部における変更の禁止
+    HWND GetHwnd() const { return hwnd; }
+
+    HINSTANCE GetHInstance() const { return w.hInstance; }
+
+private: // メンバ変数
+    // ウィンドウクラスの生成
+    WNDCLASSEX w{};
+
+    // ウィンドウハンドルの生成
+    HWND hwnd;
+};
+
+// インスタンスを取得
+Window* GetInstanceWnd();
